bc191301e307991612c3f03c3271ae1406e0b54a
BFAlex <838318741@qq.com>
2016-09-22 08:57:48 +0800

    Initial Commit

---
+++ BFUpdateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BFUpdateDemo
+//
+//  Created by Readboy_BFAlex on 16/9/22.
+//  Copyright © 2016年 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
